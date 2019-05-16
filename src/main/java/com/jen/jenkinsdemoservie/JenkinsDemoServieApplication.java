940adedee78f879de6645e1f713701f9e33e18bd
Mrinmay Santra <mrinmaysantra@Mrinmays-Air.home>
2019-05-16 20:13:35 +0100

    committed inital version

---
+++ src/main/java/com/jen/jenkinsdemoservie/JenkinsDemoServieApplication.java
@@ -0,0 +1,13 @@
+package com.jen.jenkinsdemoservie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoServieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoServieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jen/jenkinsdemoservie/JenkinsDemoServieApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jen.jenkinsdemoservie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsDemoServieApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
